eb9cc94be1ff575a195be7eb34836f7ba29dc184
ranga11 <vranga.11@gmail.com>
2018-06-11 08:54:52 -0700

    Watson Talks back

--- Chapter04/controller/restapi/features/speech_to_text.js
+++ Chapter04/controller/restapi/features/speech_to_text.js
@@ -37,5 +37,21 @@ exports.stt_token = function(req, res) {
 
 exports.tts_synthesize = function(req, res) {
   console.log("tts_synthesize entered");
+  // use the environment variables to configure text to speech
+    var ttsConfig = watson.text_to_speech(config.text_to_speech);
+    // give the synthesizer the data from the browser.
+    // you may find that you get errors if you send in an empty text string. 
+    // this can be avoided by testing req.query to see if it has any text in it
+    // that would be a good exercise to extend this code
+    var transcript = ttsConfig.synthesize(req.query);
+    transcript.on('response', function(response) {
+      if (req.query.download) {
+        response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+      }
+    });
+    // if there's an error, log it to the server console window.
+    transcript.on('error', function(error) { console.log("error encountered: "+error); next(error); });
+    // pipe sends the sound as a stream (vs a downloaded file) back to the browser
+    transcript.pipe(res);
 
 }
