0bbe6e9902ca79731ef8fce9ed4b2dca5f452905
ranga11 <vranga.11@gmail.com>
2018-06-10 19:47:27 -0700

    Learning Cognitive using Ibm Watson

--- Chapter03/controller/restapi/router.js
+++ Chapter03/controller/restapi/router.js
@@ -5,3 +5,4 @@ var speech_to_text = require('./features/speech_to_text');
 module.exports = router;
 // speech-to-text
 router.get('/api/speech-to-text/token*',speech_to_text.token);
+// line to call the server
